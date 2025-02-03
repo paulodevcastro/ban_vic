c1ec71a3d286468caa0beb49506613d89ca2376c
Gui <guizits200@gmail.com>
2025-02-03 10:50:09 -0300

    Adjust check spreadsheet

--- ban_vic_analytics/src/data_loader.py
+++ ban_vic_analytics/src/data_loader.py
@@ -2,8 +2,24 @@ import pandas as pd
 from src.config import AGENCIAS, CLIENTES, COLAB_AGENCIA, COLABORADORES, CONTAS, PROPOSTA_CREDITO, TRANSACOES
 
 def load_datas():
-    tables = pd.read_csv(AGENCIAS)
-    print(tables)
+    files = {
+        "Agencias": AGENCIAS,
+        "Clientes": CLIENTES,
+        "Colab_Agencia": COLAB_AGENCIA,
+        "Colaboradores": COLABORADORES,
+        "Contas": CONTAS,
+        "Propostas de Crédito": PROPOSTA_CREDITO,
+        "Transações": TRANSACOES
+    }
+
+    for name, path, in files.items():
+        try:
+            df = pd.read_csv(path)
+            print(f"PLANILHA: {name}")
+            print(df.head(10))
+            print("-" * 50)
+        except Exception as e:
+            print(f"Erro ao carregar {name}", e)
 
 if __name__ == "__main__":
     load_datas()
